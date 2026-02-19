6b1deac8d8351f315db5f79c91071dfe1726109f
VidoTheWiz <mcgreg500@morrisville.edu>
2026-02-18 22:23:53 -0500

    Added key-lock socket system with interaction layers

---
+++ XR_Interactions/Assets/Scripts/DrawerInteractable.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class DrawerInteractable : MonoBehaviour
+{
+    [SerializeField] XRSocketInteractor keySocket;
+    [SerializeField] bool isLocked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(keySocket != null)
+        {
+            keySocket.selectEntered.AddListener(OnDrawerUnlocked);
+            keySocket.selectExited.AddListener(OnDrawerLocked);
+        }
+    }
+
+    private void OnDrawerLocked(SelectExitEventArgs arg0)
+    {
+        isLocked = true;
+        Debug.Log("***DRAWER LOCKED***");
+    }
+
+    private void OnDrawerUnlocked(SelectEnterEventArgs arg0)
+    {
+        isLocked = false;
+        Debug.Log("***DRAWER UNLOCKED***");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
